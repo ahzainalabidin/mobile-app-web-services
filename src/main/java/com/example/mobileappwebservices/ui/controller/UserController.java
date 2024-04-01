94c32ca1ca7bcba27d3452a5605796cdb223bf29
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-01 15:07:56 +0800

    Define REST api

--- src/main/java/com/example/mobileappwebservices/ui/controller/UserController.java
+++ src/main/java/com/example/mobileappwebservices/ui/controller/UserController.java
@@ -1,10 +1,29 @@
 package com.example.mobileappwebservices.ui.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
-    
+
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create user was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+
 }
