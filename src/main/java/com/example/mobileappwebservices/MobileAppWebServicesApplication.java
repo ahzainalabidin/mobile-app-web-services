b142b69abda3ad82b1655755d81838558094075f
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-01 14:50:36 +0800

    Update MobileAppWebServicesApplication.java

--- src/main/java/com/example/mobileappwebservices/MobileAppWebServicesApplication.java
+++ src/main/java/com/example/mobileappwebservices/MobileAppWebServicesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileAppWebServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobileAppWebServicesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MobileAppWebServicesApplication.class, args);
+    }
 
 }
