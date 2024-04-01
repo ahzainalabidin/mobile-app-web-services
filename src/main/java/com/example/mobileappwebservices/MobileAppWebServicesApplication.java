e3aa6be82e8fcefa48aeee88080461430a56e49d
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-01 14:34:47 +0800

    Create a new Spring project

---
+++ src/main/java/com/example/mobileappwebservices/MobileAppWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobileappwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWebServicesApplication.class, args);
+	}
+
+}
