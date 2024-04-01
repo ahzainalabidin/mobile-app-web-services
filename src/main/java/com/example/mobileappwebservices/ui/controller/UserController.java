6184c598c95dbf1692632ea2ece5021dbf1aff9b
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-01 14:58:57 +0800

    Add annotations for controller

--- src/main/java/com/example/mobileappwebservices/ui/controller/UserController.java
+++ src/main/java/com/example/mobileappwebservices/ui/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.example.mobileappwebservices.ui.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
 public class UserController {
+    
 }
