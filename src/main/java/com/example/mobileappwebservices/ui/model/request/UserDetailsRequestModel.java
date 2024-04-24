7ac2f22809d38d83e443829e4114f42e027a7aef
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-04-24 14:08:15 +0800

    Validate email input to follow email format

--- src/main/java/com/example/mobileappwebservices/ui/model/request/UserDetailsRequestModel.java
+++ src/main/java/com/example/mobileappwebservices/ui/model/request/UserDetailsRequestModel.java
@@ -1,5 +1,6 @@
 package com.example.mobileappwebservices.ui.model.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -12,6 +13,7 @@ public class UserDetailsRequestModel {
     private String lastName;
 
     @NotNull(message = "Email cannot be null")
+    @Email
     private String email;
 
     @NotNull(message = "Password cannot be null")
