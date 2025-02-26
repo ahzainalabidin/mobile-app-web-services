266d28c5b0b61fa9067e878a20844fbd29ee0ee3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2025-02-26 21:32:00 +0800

    Handle multiple exceptions within one method

--- src/main/java/com/example/mobileappwebservices/exceptions/AppExceptionsHandler.java
+++ src/main/java/com/example/mobileappwebservices/exceptions/AppExceptionsHandler.java
@@ -29,23 +29,8 @@ public class AppExceptionsHandler extends ResponseEntityExceptionHandler {
 
     }
 
-    @ExceptionHandler(value = {NullPointerException.class})
-    public ResponseEntity<Object> handleNullPointerException(NullPointerException ex, WebRequest request) {
-
-        String errorType = ex.getClass().getName();
-        String errorMessageDescription = ex.getLocalizedMessage();
-
-        if (errorMessageDescription == null) {
-            errorMessageDescription = ex.toString();
-        }
-
-        ErrorMessage errorMessage = new ErrorMessage(new Date(), errorType, errorMessageDescription);
-        return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
-
-    }
-
-    @ExceptionHandler(value = {UserServiceException.class})
-    public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
+    @ExceptionHandler(value = {NullPointerException.class, UserServiceException.class})
+    public ResponseEntity<Object> handleSpecificException(Exception ex, WebRequest request) {
 
         String errorType = ex.getClass().getName();
         String errorMessageDescription = ex.getLocalizedMessage();
